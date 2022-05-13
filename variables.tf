9a1fd26de8dc101e7e44e25cb3e40c5dd11acbb7
wiserjuggler <varundhiman08@gmail.com>
2022-05-13 18:21:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "client_secret" {
+}
 
+variable "client_id" {
+}
+
+variable "subscription_id" {
+}
+
+variable "tenant_id" {
+}
+
+variable "resource_group_name" {
+  default     = "Terraform"
+}
+
+variable "resource_location" {
+  default     = "eastus2"
+  description = "Location of the resource group."
+}
