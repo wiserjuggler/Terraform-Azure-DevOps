3cc7363b76cfa3cc82e046325a86fcd97a6eb3c0
wiserjuggler <varundhiman08@gmail.com>
2022-05-13 23:22:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "client_secret" {
-}
-
-variable "client_id" {
-}
-
-variable "subscription_id" {
-}
-
-variable "tenant_id" {
-}
-
 variable "resource_group_name" {
   default     = "Terraform"
 }
