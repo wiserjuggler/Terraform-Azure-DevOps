4eb78f5e0db174d48d9538bf28ac0bf1da26b692
wiserjuggler <varundhiman08@gmail.com>
2022-05-14 13:17:17 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,13 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
-}
-
-backend "azurerm" {
-  resource_group_name = "terraform-tftate-rg"
-  storage_account_name = "terraformtfstate25475"
-  container_name = "tfstate"
-  key = "terraform.tfstate"
+  backend "azurerm" {
+   resource_group_name = "terraform-tftate-rg"
+   storage_account_name = "terraformtfstate25475"
+   container_name = "tfstate"
+   key = "terraform.tfstate"
+ }
 }
 
 # Configure the Microsoft Azure Provider
