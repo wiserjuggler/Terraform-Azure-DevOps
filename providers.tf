700968ff28c07b140b86d1e3d0daa61eb479a32f
wiserjuggler <varundhiman08@gmail.com>
2022-05-13 22:59:30 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,17 +1,10 @@
+# Define Terraform provider
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
+  required_version = ">= 0.12"
 }
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+# Configure the Azure provider
+provider "azurerm" { 
+  environment = "public"
+  version = ">= 2.0.0"
+  features {}  
 }
